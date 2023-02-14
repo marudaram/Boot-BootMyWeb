d047d905a188c375ee65478bbb001a8da644c548
whdmsdud77 <whdmsdud77@naver.com>
2023-02-14 18:23:42 +0900

    20230214 부트마이웹 첫번째 커밋

---
+++ src/main/java/com/eunyoung/myweb/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.eunyoung.myweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+	@GetMapping("/productReg")
+	public String reg() {
+		return "product/productReg";
+	}
+	
+	@GetMapping("/productList")
+	public String list() {
+		return "product/productList";
+	}
+	
+	@GetMapping("/productDetail")
+	public String detail() {
+		return "product/productDetail";
+	}
+	
+}
